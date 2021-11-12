d0abaf789194fe408f491cfeb4de9220e44c0f30
Albert <albert.bagil@gmail.com>
2021-11-12 17:01:51 +0700

    Tugas 6 PHP
    Tugas 6 PHP untuk dikumpulkan ke Niomic

---
+++ Tugas6_1.php
@@ -0,0 +1,11 @@
+<?php
+$habib = 21;
+$aji = $habib + 4;
+$bintan = $aji +3;
+
+echo "======================================================== <br>";
+echo "Usia Habib = $habib Tahun <br>";
+echo "Usia Aji = $aji Tahun <br>";
+echo "Usia Bintan = $bintan Tahun <br>";
+echo "======================================================== ";
+ ?>
---
+++ Tugas6_2.php
@@ -0,0 +1,15 @@
+<?php
+$jambu = 15000;
+$dus = 2000;
+$jumlahdus = 6;
+$jumlahjambu = 5;
+$jualjambu = $jumlahdus * $jumlahjambu * $jambu;
+$jualdus = 2 * $dus;
+$total = $jualjambu + $jualdus;
+
+echo "======================================================== <br>";
+echo "Harga Jambu = Rp. $jambu / Kg <br>";
+echo "Harga Kardus = Rp. $dus / pcs <br>";
+echo "Total Penjualan (Dus dan Jambu) = Rp. $total <br>";
+echo "======================================================== ";
+ ?>
